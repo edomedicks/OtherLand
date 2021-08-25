c1ab792c538fb376b7db742632c4877cfb9252dd
edomedicks <edo.medicks@bridging-it.de>
2021-08-25 13:25:03 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+// You can write your WorkAdventure script here, if any.
+// The "WA" global object is available from anywhere.
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveZone('clock', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+console.log('Welcome to Workadventure');
