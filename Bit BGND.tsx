8998cb37051d25e18452abafd718010a11bd5f04
edomedicks <edo.medicks@bridging-it.de>
2021-09-05 20:54:07 +0200

    no more bacgnd

--- Bit BGND.tsx
+++
@@ -1,4 +0,0 @@
-<? xml version = "1.0" encoding = "UTF-8" ?>
-    <tileset version="1.5" tiledversion="1.7.2" name="Bit bcgnd2" tilewidth="256" tileheight="256" tilecount="32" columns="8">
-        <image source="../../../../Pictures/Screenshots/Screenshot 2021-09-05 at 20.39.08.png" width="2500" height="2000" />
-    </tileset>
