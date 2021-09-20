b7a73c8994e7b967e41ccdc49d98ff3e7baf071d
rlueerBIT <ricardo.lueer@bridging-it.de>
2021-09-20 09:20:28 +0200

    Delete windows.tsx

--- windows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="windows" tilewidth="32" tileheight="32" tilecount="36" columns="6">
- <image source="Tilesets/windows.png" width="200" height="200"/>
-</tileset>
